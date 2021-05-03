061edc6c1891ff2e9be4e910dd0a1532cdb28432
Jakub Groncki <j.groncki@getprintbox.com>
2021-05-03 17:05:47 +0200

    Fixed test

--- test/InotifyTest.php
+++ test/InotifyTest.php
@@ -114,6 +114,6 @@ class InotifyTest extends AsyncTestCase
         \touch(self::FOO_FILE);
         \touch(self::BAR_FILE);
 
-        static::assertFalse($inotify->getPendingEvents());
+        static::assertTrue($inotify->getPendingEvents());
     }
 }
